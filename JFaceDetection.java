1b08a9f6a094d3172db4549ca22bb5886ea1d83b
Siddhesh Haldankar <89246275+siddhesh3008@users.noreply.github.com>
2022-01-13 20:29:56 +0530

    Add files via upload

---
+++ JFaceDetection.java
@@ -0,0 +1,33 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class JFaceDetection {
+
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        String imgFile = "images/TzyLTh-v5ie9SpRVJH2rHyK3OPSpSHpvYWFJ.png";
+        Mat src = Imgcodecs.imread(imgFile);
+
+        String xmlFile = "xml/lbpcascade_frontalface.xml";
+        CascadeClassifier cc = new CascadeClassifier(xmlFile);
+
+        MatOfRect faceDetection = new MatOfRect();
+        cc.detectMultiScale(src, faceDetection);
+        System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
+
+        for(Rect rect: faceDetection.toArray()) {
+            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height) , new Scalar(0, 0, 255), 3);
+        }
+
+        Imgcodecs.imwrite("images/TzyLTh-v5ie9SpRVJH2rHyK3OPSpSHpvYWFJ_out.png", src);
+        System.out.println("Image Detection Finished");
+    }
+}
